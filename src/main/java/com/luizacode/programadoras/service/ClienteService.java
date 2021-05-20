453879afc2e69e23fcd06c0930a8335b9fa6ca17
jessicamelise <jessica.melise@hotmail.com>
2021-05-20 14:05:00 -0300

    ajustes nos services para aplicar testes unitários

--- src/main/java/com/luizacode/programadoras/service/ClienteService.java
+++ src/main/java/com/luizacode/programadoras/service/ClienteService.java
@@ -5,38 +5,22 @@ import com.luizacode.programadoras.dto.ClienteDto;
 import com.luizacode.programadoras.entidade.ClienteEntidade;
 import com.luizacode.programadoras.repository.ClienteRepository;
 import org.springframework.stereotype.Service;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 
-import java.net.URI;
 import java.util.List;
 
 @Service
 @Transactional
 public class ClienteService {
 
-    @Autowired
     private ClienteRepository clienteRepository;
 
-    public ClienteService() {}
+    public ClienteService(ClienteRepository clienteRepository) {
+        this.clienteRepository = clienteRepository;
+    }
 
-    public ResponseEntity<ClienteEntidade> criarCliente(ClienteDto clienteDto) {
+    public ClienteEntidade criarCliente(ClienteDto clienteDto) {
         ClienteEntidade clienteEntidade = new ClienteEntidade(clienteDto.getNome(), clienteDto.getEndereco(), clienteDto.getEmail());
-        ClienteEntidade criarCliente = clienteRepository.save(clienteEntidade);
-
-        URI location = ServletUriComponentsBuilder
-            .fromCurrentServletMapping()
-            .path("/clientes/{id}")
-            .build()
-            .expand(criarCliente.getId())
-            .toUri();
-
-        HttpHeaders headers = new HttpHeaders();
-        headers.setLocation(location);
-        return new ResponseEntity<ClienteEntidade>(headers, HttpStatus.CREATED);
+        return clienteRepository.save(clienteEntidade);
     }
 
     public List<ClienteEntidade> listarClientes() {
